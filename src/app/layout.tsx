4672bbcb64db24d81b51c7e028b641a56a4972d4
Reda Haloubi <79601780+rhaloubi@users.noreply.github.com>
2025-12-14 20:36:49 +0100

    Enhance layout with Open Graph and Twitter metadata
    Added Open Graph and Twitter metadata for portfolio.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,41 @@ export const metadata: Metadata = {
     { rel: "icon", url: "/icon.svg", type: "image/svg+xml" },
     { rel: "icon", url: "/favicon-32x32.png", type: "image/x-icon", sizes: "32x32" },
   ],
+   openGraph: {
+    title: 'Reda Haloubi - Portfolio',
+    description:
+      'Showcasing projects and skills of Reda Haloubi, Computer Science student specializing in JavaScript, Python, React.js, and more.',
+    url: 'https://redahaloubi.com/',
+    siteName: 'Reda Haloubi Portfolio',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Reda Haloubi Portfolio',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Reda Haloubi - Portfolio',
+    description:
+      'Portfolio of Reda Haloubi, a Computer Science student and developer.',
+    images: ['/opengraph-image.png'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 const bowlbyOneSC = Bowlby_One_SC({
